5e1a2f006e22836fd890cf2d6a59f6836fbc0487
Eliseo <eliseo@Nidias-MacBook-Air.local>
2023-02-23 11:00:07 -0600

    tried fixing data storage

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -20,7 +20,7 @@ export const putDb = async (content) => {
   const store = tx.objectStore('jate');
   const request = store.put({ id: 1, value: content });
   const result = await request;
-  console.log('🚀 - data saved to the database', result);
+  console.log('🚀 - data saved to the database', result.value);
 };
 
 
